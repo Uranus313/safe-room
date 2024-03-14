ac3a6e31be5e8760d92089f95227341e97e20d44
Uranus313 <mehrbodmh82@gmail.com>
2024-03-14 15:53:26 +0330

    starting the front-end gaming site project.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+function App() {
+  return (
+    <div >
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
