041d01538dedda9133da8be7e8d29fcccef31cd7
Uranus313 <mehrbodmh82@gmail.com>
2024-03-16 12:43:04 +0330

    added a placeholder image for games with no image

--- src/functions/getCroppedImage.tsx
+++ src/functions/getCroppedImage.tsx
@@ -1,6 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp"
 function getCroppedImage(imgUrl : string){
     if (!imgUrl){
-        return ""
+        return noImage
     }
     let index = imgUrl.indexOf("media/")
     index += "media/".length
