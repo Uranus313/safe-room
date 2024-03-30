8413608b7c4cde39da9b783a8b2ae97fdd40d626
Uranus313 <mehrbodmh82@gmail.com>
2024-03-30 19:44:18 +0330

    making the error page a little prettier

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,15 +1,19 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Navbar from "./navbar";
 
 
 function ErrorPage(){
     const error = useRouteError();
     return(
         <>
+        <Navbar />
+        <Box padding={6}>
         <Heading>Oh NO!!!</Heading>
         <Text>
             {isRouteErrorResponse(error) ? "this page doesn't exist" : "seems like an error occured"}
         </Text>
+        </Box>
         </>
     );
 }
